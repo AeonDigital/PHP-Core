307d7010410b787c49eea9bb3354b94bc97320fb
Rianna Cantarelli <rianna.aeon@gmail.com>
2020-07-31 00:33:43 -0300

    refactory

--- tests/src/Objects/Types/tpTypeArrayTest.php
+++ tests/src/Objects/Types/tpTypeArrayTest.php
@@ -2,7 +2,7 @@
 declare (strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use AeonDigital\Objects\Types\tpBoolArray as tpBoolArray;
+use AeonDigital\Objects\Types\tpByteArray as tpByteArray;
 
 require_once __DIR__ . "/../../../phpunit.php";
 
@@ -19,17 +19,17 @@ class tpTypeArrayTest extends TestCase
 
     public function test_instance()
     {
-        $this->assertSame("Bool", tpBoolArray::standart()::TYPE);
-        $this->assertSame(false, tpBoolArray::standart()::IS_CLASS);
-        $this->assertSame(false, tpBoolArray::standart()::HAS_LIMIT_RANGE);
+        $this->assertSame("Byte", tpByteArray::standart()::TYPE);
+        $this->assertSame(false, tpByteArray::standart()::IS_CLASS);
+        $this->assertSame(true, tpByteArray::standart()::HAS_LIMIT_RANGE);
 
 
         // Testes de inicialização
-        $obj = new tpBoolArray();
-        $this->assertSame(tpBoolArray::standart()::TYPE, $obj->getType());
+        $obj = new tpByteArray();
+        $this->assertSame(tpByteArray::standart()::TYPE, $obj->getType());
         $this->assertSame(null, $obj->default());
-        $this->assertSame(null, $obj->min());
-        $this->assertSame(null, $obj->max());
+        $this->assertSame(-128, $obj->min());
+        $this->assertSame(127, $obj->max());
 
         $this->assertTrue($obj->isUndefined());
         $this->assertFalse($obj->isAllowNull());
@@ -46,32 +46,33 @@ class tpTypeArrayTest extends TestCase
 
 
         // Teste dos métodos específicos para uso como um array
-        $obj = new tpBoolArray(null, true);
-        $this->assertTrue($obj->isCaseSensitive());
+        $obj = new tpByteArray(null, true, true, false, null, null, null, false);
+        $this->assertFalse($obj->isCaseSensitive());
         $this->assertFalse($obj->isLocked());
         $this->assertFalse($obj->hasValue("first"));
-        $this->assertTrue($obj->setValue("first", true));
+        $this->assertTrue($obj->setValue("first", 1));
         $this->assertTrue($obj->hasValue("first"));
-        $this->assertSame(true, $obj->getValue("first"));
+        $this->assertSame(1, $obj->getValue("first"));
         $this->assertTrue($obj->unsetValue("first"));
         $this->assertFalse($obj->hasValue("first"));
 
 
-        $this->assertTrue($obj->setValue("prop1", true));
-        $this->assertTrue($obj->setValue("prop2", false));
+        $this->assertTrue($obj->setValue("PROP1", 1));
+        $this->assertTrue($obj->setValue("PROP2", 2));
         $this->assertTrue($obj->setValue("prop3", null));
-        $this->assertTrue($obj->setValue("prop4", true));
+        $this->assertTrue($obj->setValue("Prop4", 4));
+        $this->assertTrue(isset($obj["prop4"]));
         $this->assertSame(4, $obj->count());
 
         $notNull = $obj->getValuesNotNull();
-        $this->assertTrue(is_a($notNull, tpBoolArray::class));
+        $this->assertTrue(is_a($notNull, tpByteArray::class));
         $this->assertSame(3, $notNull->count());
 
         $toArray = $obj->toArray(true, true);
         $this->assertSame(3, count($toArray));
-        $this->assertSame(true, $toArray["prop1"]);
-        $this->assertSame(false, $toArray["prop2"]);
-        $this->assertSame(true, $toArray["prop4"]);
+        $this->assertSame(1, $toArray["PROP1"]);
+        $this->assertSame(2, $toArray["PROP2"]);
+        $this->assertSame(4, $toArray["Prop4"]);
 
 
         $this->assertTrue($obj->clean());
@@ -80,37 +81,35 @@ class tpTypeArrayTest extends TestCase
 
 
         // Teste do uso do objeto como um array associativo
-        $obj["P_1"] = false;
-        $obj["P_2"] = true;
-        $obj["P_3"] = false;
-        $obj["P_4"] = true;
+        $obj["P_1"] = 1;
+        $obj["P_2"] = 2;
+        $obj["P_3"] = 3;
+        $obj["P_4"] = 4;
         $obj["P_5"] = null;
         $obj["P_6"] = null;
         $obj["P_7"] = null;
         $this->assertSame(7, count($obj));
-        $this->assertSame(true, $obj["P_2"]);
+        $this->assertSame(2, $obj["P_2"]);
         unset($obj["P_7"]);
         $this->assertSame(6, count($obj));
 
         foreach ($obj as $key => $value) {
-            $k = str_replace("P_", "", $key);
+            $k = str_replace("p_", "", $key);
             if ((int)$k <= 4) {
-                if ((int)$k % 2 === 0) {
-                    $this->assertTrue($value);
-                }
-                else {
-                    $this->assertFalse($value);
-                }
+                $this->assertSame((int)$k, $value);
             }
             else {
                 $this->assertNull($value);
             }
         }
 
-
-
-        // Testar outros métodos como:
-        // isCaseSensitive
-        // lockArray
+        $obj["P_7"] = null;
+        $obj["P_8"] = null;
+        $this->assertSame(8, count($obj));
+        $obj->lockArray();
+        $obj["P_9"] = null;
+        $obj["P_10"] = null;
+        $this->assertSame(8, count($obj));
+        $this->assertTrue($obj->isLocked());
     }
 }
